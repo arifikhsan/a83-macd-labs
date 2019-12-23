2c097019b34698853b02b3ab9cca28a80f99c93d
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-12-23 21:06:23 +0700

    aaaaaaaaaaa

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-  <Title>Registration Form</Title>
+  <Title>Kegiatan harian</Title>
   <style type="text/css">
     body {
       background-color: #fff;
@@ -52,20 +52,31 @@
 </head>
 
 <body>
-  <h1>Register here!</h1>
-  <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+  <h1>Kegiatan harian!</h1>
+  <p>Apa yang ingin Anda lakukan hari ini?</p>
   <form method="post" action="index.php" enctype="multipart/form-data">
-    Name <input type="text" name="name" id="name" /></br></br>
-    Email <input type="text" name="email" id="email" /></br></br>
-    Job <input type="text" name="job" id="job" /></br></br>
-    <input type="submit" name="submit" value="Submit" />
-    <input type="submit" name="load_data" value="Load Data" />
+    <div>
+      <label for="name">Name</label><br>
+      <input type="text" name="name" id="name" />
+    </div>
+    <div>
+      <label for="name">Tempat</label><br>
+      <input type="text" name="place" id="place" />
+    </div>
+    <div>
+      <label for="name">Lokasi</label><br>
+      <input type="text" name="location" id="location" />
+    </div>
+    <div style="margin-top: 4px;">
+      <input type="submit" name="submit" value="Submit" />
+      <input type="submit" name="load_data" value="Load Data" />
+    </div>
   </form>
   <?php
-  $host = "<Nama server database Anda>";
-  $user = "<Nama admin database Anda>";
-  $pass = "<Password admin database Anda>";
-  $db = "<Nama database Anda>";
+  $host = "arifikhsanudin.database.windows.net";
+  $user = "arifikhsanudin";
+  $pass = "Apakatadunia155";
+  $db = "note";
 
   try {
     $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
@@ -74,52 +85,52 @@
     echo "Failed: " . $e;
   }
 
-  if (isset($_POST['submit'])) {
-    try {
-      $name = $_POST['name'];
-      $email = $_POST['email'];
-      $job = $_POST['job'];
-      $date = date("Y-m-d");
-      // Insert data
-      $sql_insert = "INSERT INTO Registration (name, email, job, date)
-                        VALUES (?,?,?,?)";
-      $stmt = $conn->prepare($sql_insert);
-      $stmt->bindValue(1, $name);
-      $stmt->bindValue(2, $email);
-      $stmt->bindValue(3, $job);
-      $stmt->bindValue(4, $date);
-      $stmt->execute();
-    } catch (Exception $e) {
-      echo "Failed: " . $e;
-    }
+  // if (isset($_POST['submit'])) {
+  //   try {
+  //     $name = $_POST['name'];
+  //     $email = $_POST['email'];
+  //     $job = $_POST['job'];
+  //     $date = date("Y-m-d");
+  //     // Insert data
+  //     $sql_insert = "INSERT INTO Registration (name, email, job, date)
+  //                       VALUES (?,?,?,?)";
+  //     $stmt = $conn->prepare($sql_insert);
+  //     $stmt->bindValue(1, $name);
+  //     $stmt->bindValue(2, $email);
+  //     $stmt->bindValue(3, $job);
+  //     $stmt->bindValue(4, $date);
+  //     $stmt->execute();
+  //   } catch (Exception $e) {
+  //     echo "Failed: " . $e;
+  //   }
 
-    echo "<h3>Your're registered!</h3>";
-  } else if (isset($_POST['load_data'])) {
-    try {
-      $sql_select = "SELECT * FROM Registration";
-      $stmt = $conn->query($sql_select);
-      $registrants = $stmt->fetchAll();
-      if (count($registrants) > 0) {
-        echo "<h2>People who are registered:</h2>";
-        echo "<table>";
-        echo "<tr><th>Name</th>";
-        echo "<th>Email</th>";
-        echo "<th>Job</th>";
-        echo "<th>Date</th></tr>";
-        foreach ($registrants as $registrant) {
-          echo "<tr><td>" . $registrant['name'] . "</td>";
-          echo "<td>" . $registrant['email'] . "</td>";
-          echo "<td>" . $registrant['job'] . "</td>";
-          echo "<td>" . $registrant['date'] . "</td></tr>";
-        }
-        echo "</table>";
-      } else {
-        echo "<h3>No one is currently registered.</h3>";
-      }
-    } catch (Exception $e) {
-      echo "Failed: " . $e;
-    }
-  }
+  //   echo "<h3>Your're registered!</h3>";
+  // } else if (isset($_POST['load_data'])) {
+  //   try {
+  //     $sql_select = "SELECT * FROM Registration";
+  //     $stmt = $conn->query($sql_select);
+  //     $registrants = $stmt->fetchAll();
+  //     if (count($registrants) > 0) {
+  //       echo "<h2>People who are registered:</h2>";
+  //       echo "<table>";
+  //       echo "<tr><th>Name</th>";
+  //       echo "<th>Email</th>";
+  //       echo "<th>Job</th>";
+  //       echo "<th>Date</th></tr>";
+  //       foreach ($registrants as $registrant) {
+  //         echo "<tr><td>" . $registrant['name'] . "</td>";
+  //         echo "<td>" . $registrant['email'] . "</td>";
+  //         echo "<td>" . $registrant['job'] . "</td>";
+  //         echo "<td>" . $registrant['date'] . "</td></tr>";
+  //       }
+  //       echo "</table>";
+  //     } else {
+  //       echo "<h3>No one is currently registered.</h3>";
+  //     }
+  //   } catch (Exception $e) {
+  //     echo "Failed: " . $e;
+  //   }
+  // }
   ?>
 </body>
 
